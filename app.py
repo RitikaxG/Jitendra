8dd40c5c18e4cfd2c63c999715505367d175a552
Ritika Gupta <97238841+RitikaxG@users.noreply.github.com>
2024-01-09 12:40:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 load_dotenv()
 
-openai_api_key = "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"
+openai_api_key = "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"
 
 def chat_with_csv(df,prompt):
     llm = OpenAI(api_token=openai_api_key)
